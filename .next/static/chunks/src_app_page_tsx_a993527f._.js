798e3b10ccdaf2e0aaa68b1b23af5b8598014978
SageOfficialz <sageofficialzero@gmail.com>
2025-08-12 00:55:39 +0800

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1754931339096

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_qr-scanner_qr-scanner-worker_min_30564c4a.js",
+  "static/chunks/node_modules_5801d249._.js",
+  "static/chunks/src_bad69cfd._.js"
+],
+    source: "dynamic"
+});
